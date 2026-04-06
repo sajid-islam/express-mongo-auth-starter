68928079c9c89dee28c590ebee38089af76784c9
sajid-islam <sajidislam941@gmail.com>
2026-04-06 19:45:58 +0600

    fix: remove default values for Cloudinary configuration to enforce environment variable usage

--- src/config/cloudinary.ts
+++ src/config/cloudinary.ts
@@ -4,9 +4,9 @@ import { configDotenv } from 'dotenv';
 configDotenv();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'demo',
-  api_key: process.env.CLOUDINARY_API_KEY || 'demo_key',
-  api_secret: process.env.CLOUDINARY_API_SECRET || 'demo_secret',
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
+  api_key: process.env.CLOUDINARY_API_KEY!,
+  api_secret: process.env.CLOUDINARY_API_SECRET!,
 });
 
 export default cloudinary;
